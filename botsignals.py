c7c2eff2e1b21d3f880c654fe684f132eb306461
N <nik.trdr@gmail.com>
2024-10-05 14:42:01 +0300

    Исправлен токен бота и обновлен код

--- botsignals.py
+++ botsignals.py
@@ -1,9 +1,10 @@
 from telegram import Update, Bot
 from telegram.ext import Application, CommandHandler, CallbackContext
 import requests
+import asyncio
 
 # Токен Telegram бота
-API_KEYS = ['5EUEU0UEJY0PGTCN ']
+API_KEYS = ['5EUEU0UEJY0PGTCN']
 API_KEY = API_KEYS[0]
 
 # Список активов
@@ -20,7 +21,7 @@ assets = {
     "bitcoin": ("BTC", "USD"),
 }
 
-def get_currency_data(from_symbol, to_symbol=None):
+async def get_currency_data(from_symbol, to_symbol=None):
     """
     Получение данных о валютной паре или активе с Alpha Vantage API.
     """
@@ -43,31 +44,31 @@ def get_currency_data(from_symbol, to_symbol=None):
         return price, None
     return None, "Не удалось получить данные"
 
-def send_signal(update: Update, context: CallbackContext, asset: str):
+async def send_signal(update: Update, context: CallbackContext, asset: str):
     """
     Отправка актуальной цены актива в ответ на команду.
     """
     if asset in assets:
         from_symbol, to_symbol = assets[asset]
-        price, error = get_currency_data(from_symbol, to_symbol)
+        price, error = await get_currency_data(from_symbol, to_symbol)
 
         if error:
-            update.message.reply_text(error)
+            await update.message.reply_text(error)
         else:
             pair_symbol = f"{from_symbol}/{to_symbol}" if to_symbol else from_symbol
             signal_message = (f"🔥Актуальная цена для {pair_symbol}: {price}")
-            update.message.reply_text(signal_message)
+            await update.message.reply_text(signal_message)
     else:
-        update.message.reply_text(f"Актив {asset} не поддерживается.")
+        await update.message.reply_text(f"Актив {asset} не поддерживается.")
 
-def handle_command(update: Update, context: CallbackContext):
+async def handle_command(update: Update, context: CallbackContext):
     """
     Универсальный обработчик для команд актива.
     """
     command = update.message.text[1:]  # Получаем название актива (убираем "/")
-    send_signal(update, context, command)
+    await send_signal(update, context, command)
 
-def main():
+async def main():
     token = '7449818362:AAHrejKv90PyRkrgMTdZvHzT9p44ePlZYcg'
     application = Application.builder().token(token).build()
 
@@ -76,7 +77,9 @@ def main():
         application.add_handler(CommandHandler(asset, handle_command))
 
     # Запуск бота
-    application.run_polling()
+    await application.start()
+    await application.updater.start_polling()
+    await application.wait_for_shutdown()
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
